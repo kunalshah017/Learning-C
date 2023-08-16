f43ba3bf314366c1ac68ff3ce63b353babc343a2
Kunal Shah <kunalmanishshah@gmail.com>
2023-08-16 20:55:28 +0530

    Updated till 16-08-23

--- College Lecture Codes & HW/HW/16-Aug-23/square_of_first_5_no.c
+++ College Lecture Codes & HW/HW/16-Aug-23/square_of_first_5_no.c
@@ -2,9 +2,11 @@
 #include <stdio.h>
 void main()
 {
-    int i;
-    for (i = 1; i <= 5; i++)
+    int i = 1;
+    while (i <= 5)
     {
         printf("Square of %d is = %d\n", i, i * i);
+        i++;
     }
+    getch();
 }
