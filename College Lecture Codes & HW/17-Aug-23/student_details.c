0bf96e00e9988f033f9338d5e26c8f3a7ce89643
KsDivine23 <ksdivine2023@outlook.com>
2023-08-17 16:21:22 +0530

    first commit from clg

---
+++ College Lecture Codes & HW/17-Aug-23/student_details.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <conio.h> 
+
+int main(){
+    char studentname[100];
+    int roll_no;
+
+    printf("Enter Student Name = ");
+    scanf("%c",&studentname);
+
+printf("Enter Student Roll No = ");
+    scanf("%d",&roll_no);
+
+
+    printf("Student name - %c\nStudent Roll no - %d",studentname,roll_no);
+
+ getch();   
+}
